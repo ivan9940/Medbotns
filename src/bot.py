7fc08458ea6fcc8171e71dcefdc1ef48c6b137ac
Степан Буян <Stipundrik@gmail.com>
2025-03-26 22:49:57 +0300

    Update

--- src/bot.py
+++ src/bot.py
@@ -7,6 +7,9 @@ from src.config import settings
 TOKEN = settings.TELEGRAM_BOT_API_TOKEN
 bot = telebot.TeleBot(TOKEN)
 
+with open("../ava.jpg", "rb") as photo:
+    bot.set_chat_photo(chat_id=bot.get_me().id, photo=photo)
+
 # Загружаем данные из Excel
 FILE_PATH = "../Medbotsheets.xlsx"  # Укажите путь к вашему файлу
 df = pd.read_excel(FILE_PATH, header=None)
@@ -22,13 +25,19 @@ def find_closest_match(query):
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Привет! Отправьте мне название лекарства, и я предоставлю вам информацию о нём.")
+    bot.send_message(message.chat.id, "Привет! Отправьте мне название лекарства, и я предоставлю вам информацию о нём. Чтобы узнать, инструкции к каким лекарствам есть в базе чат-бота используйте команду /list")
+
+@bot.message_handler(commands=['list'])
+def list_medications(message):
+    bot.send_message(message.chat.id, "Список лекарств в базе чат-бота:\n" + medications['алфавитный указатель лекарств'])
+
 
 @bot.message_handler(content_types=['text'])
 def send_medication_info(message):
     query = message.text.strip().lower()
     description = find_closest_match(query)
-    bot.send_message(message.chat.id, description)
+    if description != "алфавитный указатель лекарств":
+        bot.send_message(message.chat.id, description)
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
